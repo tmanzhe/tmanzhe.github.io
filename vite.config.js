d86f8affc97557299fabacd1ca5eb15b4a2d738b
tmanzhe <97918794+tmanzhe@users.noreply.github.com>
2024-09-22 18:10:03 -0400

    assetsInclude: ['**/*.JPG', '**/*.jpg'], // Include both uppercase and lowercase

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/tutorial/",
   plugins: [react()],
+  assetsInclude: ['**/*.JPG', '**/*.jpg'], // Include both uppercase and lowercase
+
 })
