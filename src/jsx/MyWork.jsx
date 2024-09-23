33d2e0b1122f95e2456ea8068877189beeb6ef1d
tmanzhe <97918794+tmanzhe@users.noreply.github.com>
2024-09-22 20:03:50 -0400

    updated mywork

--- src/jsx/MyWork.jsx
+++ src/jsx/MyWork.jsx
@@ -1,5 +1,13 @@
 import React, { useEffect, useRef } from 'react';
 import ITMZLogo from '../assets/company logo/ITMZ.png';
+import Clicker from '../assets/demo/clicker.png';
+import VirtualMouse from '../assets/demo/virtual-mouse.png';
+import DriveOne from '../assets/demo/drive-1.png';
+import DriveTwo from '../assets/demo/drive-2.png';
+import Mad from '../assets/demo/mad.png'
+import WebsiteCode from '../assets/demo/website-code.jpg'
+
+
 import 'slick-carousel/slick/slick.css'; 
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
@@ -56,10 +64,15 @@ function MyWork() {
                 <div className="timeline-component timeline-content">
                     <h2>mobile dev intern @ ITMZ</h2>
                     <small> june 2024 - present </small>
-                    <p>during my internship at itmz, i focused on enhancing the front-end user experience by designing and deploying innovative ui features in a timely and organized manner within a fast-paced startup environment using <b>flutter and dart. </b> these contributions significantly improved product capabilities and optimized user interactions.</p>
-                    <p>i worked with <b>aws dynamodb and aws s3</b> to develop and optimize data retrieval and display systems, managing custom content permissions to ensure seamless integration with front-end components, secure data handling, and enhanced data accessibility.</p>
-                    <p>i was tasked with creating numerous features from scratch, identifying and solving bugs, and collaborating with cross-functional teams to implement performance enhancements that drove efficiency and increased user engagement.</p>
-                    <p>fun fact: since our app is powered by gemini, we participated in the gemini ai competition. we are currently in the process of awaiting results</p>
+
+                    <p>during my internship at itmz, i enhanced user experiences by designing and deploying innovative <strong>ui features</strong> with <strong>flutter</strong> and <strong>dart</strong>. these contributions significantly improved product capabilities.</p>
+
+                    <p>i collaborated with backend components using <strong>aws dynamodb</strong> and <strong>aws s3</strong>. my focus was on connecting and tweaking systems for better data retrieval and managing custom content permissions for secure data handling.</p>
+
+                    <p>i created features from scratch and identified bugs, including issues like identifying and optimizing internet connectivity. i also collaborated with cross-functional teams to implement efficiency enhancements.</p>
+
+                    <p>fun fact: as our app is powered by <strong>gemini</strong>, we participated in the <strong>gemini ai competition</strong> and are currently awaiting the results.</p>
+
                     <div className="images">
                         <Slider {...settings}>
                             <div>
@@ -73,16 +86,18 @@ function MyWork() {
                 <div className="timeline-component timeline-content">
                     <h2>Self-Driving Car Using Neural Network</h2>
                     <small> may 2024 - present </small>
-                    <p>i started this project due to my keen interest in ai cars in shanghai. fortunately, by following resources online, i was able to develop a <b>self-driving car simulation</b> from using no libraries in <b>javascript</b>, incorporating essential components such as <b>car driving physics</b>, which also involved implementing core movement and control systems.</p> 
-                    <p>it includes <b>driving environments</b> and <b>road structures</b>, along with <b>artificial sensors</b> for data collection and decision-making. additionally, <b>algorithms for collision detection</b> were implemented, and various <b>traffic scenarios</b> were simulated to test car behavior under different conditions.</p> 
-                    <p>a key aspect of the project was the implementation of a <b>neural network</b> for <b>autonomous driving</b>, which involved visualizing its operations and tracking performance improvements. i utilized <b>parallel computing</b> and <b>genetic algorithms</b> to enhance the learning process.</p> 
-                    <p>currently, i am focused on advancing this project by creating a sophisticated <b>virtual environment</b> from scratch using <b>html</b>, <b>css</b>, and <b>javascript</b>. this includes implementing <b>dijkstra’s algorithm</b> for efficient pathfinding and integrating <b>real-world data</b> to create realistic cityscapes.</p>
+
+                    <p>i started this project after visiting shenzhen and seeing self-driving cars. inspired i used online resources to help me replicate a <strong>self-driving car simulation</strong> in <strong>javascript</strong> without any libraries. this involved incorporating essential components like <strong>car driving physics</strong> and core movement control systems.</p>  
+                    <p>the simulation features <strong>driving environments</strong> and <strong>road structures</strong>. it includes <strong>artificial sensors</strong> for data collection and decision-making. i implemented <strong>collision detection algorithms</strong> and simulated various <strong>traffic scenarios</strong> to test car behavior under different conditions.</p> 
+                    <p>a key aspect of the project was implementing a <strong>neural network</strong> for <strong>autonomous driving</strong>. i visualized its operations and tracked performance improvements. additionally, i utilized <strong>parallel computing</strong> and <strong>genetic algorithms</strong> to enhance the learning process.</p> 
+                    <p>currently, i am advancing this project by creating a sophisticated <strong>virtual environment</strong> from scratch using <strong>html</strong>, <strong>css</strong>, and <strong>javascript</strong>. this includes implementing <strong>dijkstra’s algorithm</strong> for efficient pathfinding and integrating <strong>real-world data</strong> to create realistic cityscapes.</p>
+
                     <div className="images">
                         <Slider {...settings}>
                             <div>
-                                <img src={ITMZLogo} alt="ITMZ Logo" className="carousel-image" />
+                                <img src={DriveOne} alt="ITMZ Logo" className="carousel-image" />
                             </div>
-                                <img src={ITMZLogo} alt="ITMZ Logo" className="carousel-image" />
+                                <img src={DriveTwo} alt="ITMZ Logo" className="carousel-image" />
                         </Slider>
                     </div>
                 </div>
@@ -103,35 +118,31 @@ function MyWork() {
                 <div className="timeline-component timeline-content">
                     <h2>Virtual Mouse</h2>
                     <small> april 2024 - present </small>
-                    <p> this project utilizes <b>opencv</b> and <b>mediapipe</b> to detect the location of the hand and is coded in python. by tracking the index finger, the system calculates the <b>euclidean distance</b> between specific points on the hand in each frame. when the distance meets a predefined threshold, a command can be executed.</p> 
+                    <p> this project utilizes <b>opencv</b> and <b>mediapipe</b> to detect the location of the hand and is coded in python. by tracking the index finger, the program calculates the <b>euclidean distance</b> between specific points on the hand in each frame. when the distance meets a predefined threshold, a command can be executed.</p> 
                     <p>the implementation involves creating a mouse controller instance and capturing real-time video streams to identify hand landmarks. gestures such as left-click, right-click, double-click, and screenshot are recognized based on specific angles and distances between the thumb and index finger.</p>
                     <p>since it was one of my first projects involving <b>ai</b> and <b>human-computer interaction (hci)</b>, the final product of the virtual mouse is functional, but not in its final stages.</p>
                     <p>in the future, i want to be able to make this interactive in 3d enviroments like unity</p>
                     <div className="images">
                         <Slider {...settings}>
                             <div>
-                                <img src={ITMZLogo} alt="ITMZ Logo" className="carousel-image" />
+                                <img src={VirtualMouse} alt="ITMZ Logo" className="carousel-image" />
                             </div>
-                                <img src={ITMZLogo} alt="ITMZ Logo" className="carousel-image" />
+                                <img src={Clicker} alt="ITMZ Logo" className="carousel-image" />
                         </Slider>
                     </div>
                 
                 </div>
 
                 <div className="timeline-component timeline-content">
-                    <h2>IDK Last Project</h2>
-                    <small> 2024-2024 </small>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni reiciendis, iusto id incidunt dolores blanditiis perferendis rerum nihil autem repudiandae, et distinctio? Possimus neque quidem ut dolorem, facere repellendus esse.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni reiciendis, iusto id incidunt dolores blanditiis perferendis rerum nihil autem repudiandae, et distinctio? Possimus neque quidem ut dolorem, facere repellendus esse.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni reiciendis, iusto id incidunt dolores blanditiis perferendis rerum nihil autem repudiandae, et distinctio? Possimus neque quidem ut dolorem, facere repellendus esse.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni reiciendis, iusto id incidunt dolores blanditiis perferendis rerum nihil autem repudiandae, et distinctio? Possimus neque quidem ut dolorem, facere repellendus esse.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni reiciendis, iusto id incidunt dolores blanditiis perferendis rerum nihil autem repudiandae, et distinctio? Possimus neque quidem ut dolorem, facere repellendus esse.</p>
+                    <h2>my personal website</h2>
+                    <small> present </small>
+                    <p>this website was built with <strong>react.js</strong>, <strong>html</strong>, <strong>css</strong>, and a whole lot of blood, sweat, and tears. in the future, i’m planning to lock myself in a room and force myself to make it look cooler using <strong>three.js</strong>. </p>
                     <div className="images">
                         <Slider {...settings}>
                             <div>
-                                <img src={ITMZLogo} alt="ITMZ Logo" className="carousel-image" />
+                                <img src={WebsiteCode} alt="ITMZ Logo" className="carousel-image" />
                             </div>
-                                <img src={ITMZLogo} alt="ITMZ Logo" className="carousel-image" />
+                                <img src={Mad} alt="ITMZ Logo" className="carousel-image" />
                         </Slider>
                     </div>
                 </div>
